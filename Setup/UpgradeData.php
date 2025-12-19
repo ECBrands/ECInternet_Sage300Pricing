ce65badd6628d0e27d81c00a8feafddec0f0c1b9
Randy Esplin <randy.esplin@gmail.com>
2025-12-19 11:08:13 -0800

    UpgradeData - Fix issue with Invalid entity_type

--- Setup/UpgradeData.php
+++ Setup/UpgradeData.php
@@ -144,7 +144,7 @@ class UpgradeData implements UpgradeDataInterface
             /** @var \Magento\Eav\Model\Entity\Attribute\AbstractAttribute $attribute */
             $attribute = $customerSetup->getEavConfig()
                 ->getAttribute(Customer::ENTITY, 'currency_code')
-                ->setData([
+                ->addData([
                     'attribute_set_id'   => $attributeSetId,
                     'attribute_group_id' => $attributeGroupId,
                     'used_in_forms'      => [

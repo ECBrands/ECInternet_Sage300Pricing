a59bd5ced078404639c09c4cf09d7d506af064d9
Randy Esplin <randy.esplin@gmail.com>
2026-05-27 17:09:58 -0700

    Convert to data patches, add tests

---
+++ Setup/Patch/Data/AddCurrencyCodeAttributeToCustomer.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Copyright (C) EC Brands Corporation - All Rights Reserved
+ * Contact Licensing@ECInternet.com for use guidelines
+ */
+declare(strict_types=1);
+
+namespace ECInternet\Sage300Pricing\Setup\Patch\Data;
+
+use Magento\Customer\Model\Customer;
+use Magento\Customer\Model\ResourceModel\Attribute as AttributeResource;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class AddCurrencyCodeAttributeToCustomer implements DataPatchInterface
+{
+    /**
+     * @var \Magento\Customer\Model\ResourceModel\Attribute
+     */
+    private $attributeResource;
+
+    /**
+     * @var \Magento\Customer\Setup\CustomerSetupFactory
+     */
+    private $customerSetupFactory;
+
+    /**
+     * @var \Magento\Framework\Setup\ModuleDataSetupInterface
+     */
+    private $setup;
+
+    public function __construct(
+        AttributeResource $attributeResource,
+        CustomerSetupFactory $customerSetupFactory,
+        ModuleDataSetupInterface $setup
+    ) {
+        $this->attributeResource    = $attributeResource;
+        $this->customerSetupFactory = $customerSetupFactory;
+        $this->setup                = $setup;
+    }
+
+    public static function getDependencies(): array
+    {
+        return [AddCustomerTypeAttributeToCustomer::class];
+    }
+
+    public function getAliases(): array
+    {
+        return [];
+    }
+
+    /**
+     * @return void
+     * @throws \Exception
+     * @throws \Magento\Framework\Exception\AlreadyExistsException
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function apply(): void
+    {
+        $this->setup->getConnection()->startSetup();
+
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $this->setup]);
+        $customerSetup->addAttribute(Customer::ENTITY, 'currency_code', [
+            'type'         => 'varchar',
+            'label'        => 'Currency Code',
+            'input'        => 'text',
+            'required'     => false,
+            'visible'      => true,
+            'user_defined' => false,
+            'position'     => 999,
+            'system'       => 0,
+        ]);
+
+        if ($attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'currency_code')) {
+            $attribute->setData('used_in_forms', ['adminhtml_customer']);
+            $this->attributeResource->save($attribute);
+        }
+
+        $this->setup->getConnection()->endSetup();
+    }
+}
---
+++ Setup/Patch/Data/AddDefaultPriceListCodeAttributeToProduct.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Copyright (C) EC Brands Corporation - All Rights Reserved
+ * Contact Licensing@ECInternet.com for use guidelines
+ */
+declare(strict_types=1);
+
+namespace ECInternet\Sage300Pricing\Setup\Patch\Data;
+
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class AddDefaultPriceListCodeAttributeToProduct implements DataPatchInterface
+{
+    /**
+     * @var \Magento\Eav\Setup\EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * @var \Magento\Framework\Setup\ModuleDataSetupInterface
+     */
+    private $setup;
+
+    public function __construct(
+        EavSetupFactory $eavSetupFactory,
+        ModuleDataSetupInterface $setup
+    ) {
+        $this->eavSetupFactory = $eavSetupFactory;
+        $this->setup           = $setup;
+    }
+
+    public static function getDependencies(): array
+    {
+        return [AddCurrencyCodeAttributeToCustomer::class];
+    }
+
+    public function getAliases(): array
+    {
+        return [];
+    }
+
+    /**
+     * @return void
+     * @throws \Exception
+     */
+    public function apply(): void
+    {
+        $this->setup->getConnection()->startSetup();
+
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->setup]);
+        $eavSetup->addAttribute(Product::ENTITY, 'default_price_list_code', [
+            'type'                    => 'varchar',
+            'label'                   => 'Default Price List Code',
+            'global'                  => ScopedAttributeInterface::SCOPE_GLOBAL,
+            'visible'                 => true,
+            'required'                => false,
+            'user_defined'            => false,
+            'default'                 => '',
+            'searchable'              => false,
+            'filterable'              => false,
+            'comparable'              => false,
+            'visible_on_front'        => false,
+            'used_in_product_listing' => false,
+            'unique'                  => false,
+            'apply_to'                => '',
+            'nullable'                => true,
+        ]);
+
+        $this->setup->getConnection()->endSetup();
+    }
+}
